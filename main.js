19d4843de2f7eb773d074bf1a9a2e1f137f186de
Rafael Ferrarezi <rafa.designdigital@gmail.com>
2022-08-28 20:30:30 -0300

    Fixing table with content to see what im doing

--- main.js
+++ main.js
@@ -17,14 +17,12 @@ let alturaFrontao = 0.15
 let quantidadeTampo = comprimentoBancada * larguraBancada
 
 tabela.innerHTML +=
-  '<th scope="row">1</th><td> Quantidade m² tampo: </td> <td> ' +
-  quantidadeTampo +
-  '</td>'
+  '<th scope="row">1</th><td> Tampo: </td> <td> ' + quantidadeTampo + '</td>'
 
 let faixaEngrosso = (3 * comprimentoBancada + 3 * larguraBancada) * larguraFaixa
 
 tabela.innerHTML +=
-  '<th scope="row">2</th><td> Faixa:: </td> <td> ' + faixaEngrosso + '</td>'
+  '<th scope="row">2</th><td> Faixa: </td> <td> ' + faixaEngrosso + '</td>'
 
 let saiaBancada = (comprimentoBancada + larguraBancada) * alturaSaia
 
