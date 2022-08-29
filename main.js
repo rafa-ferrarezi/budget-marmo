a066a68c685e9d3fb594c6382317c139be856cc7
Rafael Ferrarezi <rafa.designdigital@gmail.com>
2022-08-28 21:12:22 -0300

    Fixing the numbers after comma

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+function arredondar(valor) {
+  return Math.round(valor * 100) / 100
+}
+
 let teste = '<p>Bancada da Cuba de Cozinha de 200x60 </p>'
 
 let show = document.querySelector('.display')
@@ -8,40 +12,51 @@ show.innerHTML = teste
 
 // Inicio do app
 
-let comprimentoBancada = 2
-let larguraBancada = 0.6
+let comprimentoBancada = 3
+let larguraBancada = 0.62
 let alturaSaia = 0.1
 let larguraFaixa = 0.05
 let alturaFrontao = 0.15
 
-let quantidadeTampo = comprimentoBancada * larguraBancada
+let quantidadeTampo = arredondar(comprimentoBancada * larguraBancada)
 
 tabela.innerHTML +=
   '<th scope="row">1</th><td> Tampo: </td> <td> ' + quantidadeTampo + '</td>'
 
-let faixaEngrosso = (3 * comprimentoBancada + 3 * larguraBancada) * larguraFaixa
+let faixaEngrosso = arredondar(
+  (3 * comprimentoBancada + 3 * larguraBancada) * larguraFaixa
+)
 
 tabela.innerHTML +=
   '<th scope="row">2</th><td> Faixa: </td> <td> ' + faixaEngrosso + '</td>'
 
-let saiaBancada = (comprimentoBancada + larguraBancada) * alturaSaia
+let saiaBancada = arredondar((comprimentoBancada + larguraBancada) * alturaSaia)
 
 tabela.innerHTML +=
   '<th scope="row">3</th><td> Saia: </td> <td> ' + saiaBancada + '</td>'
 
-let meiaEsquadria = saiaBancada / alturaSaia
+let meiaEsquadria = arredondar(saiaBancada / alturaSaia)
 
 tabela.innerHTML +=
   '<th scope="row">4</th><td> Meia Esquadria: </td> <td> ' +
   meiaEsquadria +
   '</td>'
 
-let frontaoBancada = meiaEsquadria * alturaFrontao
+let frontaoBancada = arredondar(meiaEsquadria * alturaFrontao)
 
 tabela.innerHTML +=
   '<th scope="row">5</th><td> Frontão: </td> <td> ' + frontaoBancada + '</td>'
 
-let rebaixoFrontao = frontaoBancada / alturaFrontao
+let rebaixoFrontao = arredondar(frontaoBancada / alturaFrontao)
 
 tabela.innerHTML +=
   '<th scope="row">6</th><td> Rebaixo: </td> <td> ' + rebaixoFrontao + '</td>'
+
+let totalMaterial = arredondar(
+  quantidadeTampo + faixaEngrosso + saiaBancada + frontaoBancada
+)
+
+tabela.innerHTML +=
+  '<th scope="row">#</th><td> Total de material: </td> <td> ' +
+  totalMaterial +
+  ' m² </td>'
